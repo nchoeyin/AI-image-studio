45733c88e4c20b5ff3f9318ca061cbc707a1a4e8
Ngawang Choeying <ngawangchoeying303@gmail.com>
2026-03-08 11:21:38 +0530

    [frontend::comp::signup] footer comp initialised and populated

--- frontend/src/app/apps/ai-image-studio/landing-page/landing-page.component.ts
+++ frontend/src/app/apps/ai-image-studio/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../components/navbar/navbar.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
 })
